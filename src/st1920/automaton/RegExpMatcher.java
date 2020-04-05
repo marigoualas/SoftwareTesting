4edecebb6557842672d2598709b4a75798677b86
marigoualas <marios.ana97@gmail.com>
2020-04-06 00:05:39 +0100

    Update on RegExpMatcher

--- src/st1920/automaton/RegExpMatcher.java
+++ src/st1920/automaton/RegExpMatcher.java
@@ -5,104 +5,260 @@ package st1920.automaton;
  */
 public class RegExpMatcher {
 
-    public static MatchString makeRepeatZeroOrOne(MatchString p) {
-        return new MatchString(p + "?");
+    public static REstring makeRepeatZeroOrOne(MatchString p) {
+        return new REstring(p + "?");
     }
 
-    public static MatchString makeRepeatZeroOrMore(MatchString p) {
-        return new MatchString(p + "*");
+    public static REstring makeRepeatZeroOrMore(MatchString p) {
+        return new REstring(p + "*");
     }
 
-    public static MatchString makeRepeatOneOrMore(MatchString p) {
-        return new MatchString(p + "+");
+    public static REstring makeRepeatOneOrMore(MatchString p) {
+        return new REstring(p + "+");
     }
 
-    public static MatchString makeOr(MatchString p, MatchString p2) {
-        return new MatchString(p + "|" + p2);
+    public static REstring makeOr(MatchString p, MatchString p2) {
+        return new REstring(p + "|" + p2);
     }
 
-    public static MatchString makeAnd(MatchString p, MatchString p2) {
-        return new MatchString(p + "&" + p2);
+    public static REstring makeAnd(MatchString p, MatchString p2) {
+        return new REstring(p + "&" + p2);
     }
 
-    public static MatchString makeFollowedBy(MatchString p, MatchString p2) {
-        return new MatchString(p + "" + p2);
+    public static REstring makeFollowedBy(MatchString p, MatchString p2) {
+        return new REstring(p + "" + p2);
     }
 
-    public static MatchString makeOccurences(MatchString p, MatchString occ) {
-        return new MatchString(p + "{" + occ + "}");
+    public static REstring makeOccurences(MatchString p, MatchString occ) {
+        return new REstring(p + "{" + occ + "}");
     }
 
-    public static MatchString makeOccurencesAndMore(MatchString p, MatchString occ) {
-        return new MatchString(p + "{" + occ + ",}");
+    public static REstring makeOccurencesAndMore(MatchString p, MatchString occ) {
+        return new REstring(p + "{" + occ + ",}");
     }
 
-    public static MatchString makeOccurencesFromTo(MatchString p, MatchString occ_from, MatchString occ_to) {
-        return new MatchString(p + "{" + occ_from + "," + occ_to + "}");
+    public static REstring makeOccurencesFromTo(MatchString p, MatchString occ_from, MatchString occ_to) {
+        return new REstring(p + "{" + occ_from + "," + occ_to + "}");
     }
 
-    public static MatchString makeNot(MatchString p) {
-        return new MatchString("~" + p);
+    public static REstring makeNot(MatchString p) {
+        return new REstring("~" + p);
     }
 
-    public static MatchString makePattern(MatchString p) {
-        return new MatchString("[" + p + "]");
+    public static REstring makePattern(MatchString p) {
+        return new REstring("[" + p + "]");
     }
 
-    public static MatchString makeNotPattern(MatchString p) {
-        return new MatchString("[~" + p + "]");
+    public static REstring makeNotPattern(MatchString p) {
+        return new REstring("[~" + p + "]");
     }
 
-    public static MatchString makeAnyChar() {
+    public static REstring makeAnyChar() {
+        return new REstring(".");
+    }
+    
+    public static MatchString makeDot() {
         return new MatchString(".");
     }
 
-    public static MatchString makeNotAnything() {
+    public static REstring makeNotAnything() {
+        return new REstring("#");
+    }
+    
+    public static MatchString makeHash() {
         return new MatchString("#");
     }
 
-    public static MatchString makeAnything() {
+    public static REstring makeAnything() {
+        return new REstring("@");
+    }
+    
+    public static MatchString makeAt() {
         return new MatchString("@");
     }
 
-    public static MatchString makeString(MatchString p) {
-        return new MatchString("\"" + p + "\"");
+    public static REstring makeString(MatchString p) {
+        return new REstring("\"" + p + "\"");
     }
 
-    public static MatchString makeRange(MatchString num1, MatchString num2) {
-        return new MatchString("<" + num1 + "-" + num2 + ">");
+    public static REstring makeRange(MatchString num1, MatchString num2) {
+        return new REstring("<" + num1 + "-" + num2 + ">");
     }
 
-    public static String makeAlpha() {
-        return "a";
+    public static MatchString makeAlpha() {
+        return new MatchString("a");
     }
 
-    public static String makeNum() {
-        return "1";
-    }
-    
-    public static String makeOrSymbol() {
-        return "|";
-    }
-    
-    public static String makeAndSymbol() {
-        return "&";
-    }
-    
-    public static String makeStar() {
-        return "*";
-    }
-    
-    public static String makePlus() {
-        return "+";
+    public static MatchString makeNum() {
+        return new MatchString("1");
     }
 
-    public static String makeConcatenation(String l, String r) {
-        return l + r;
+    public static MatchString makeB() {
+        return new MatchString("b");
     }
 
-    public static boolean matches(String m, MatchString re) {
-        return matches(m, re.getString());
+    public static MatchString makeC() {
+        return  new MatchString("c");
+    }
+
+    public static MatchString makeD() {
+        return new MatchString("d");
+    }
+
+    public static MatchString makeE() {
+        return new MatchString("e");
+    }
+
+    public static MatchString makeF() {
+        return new MatchString("f");
+    }
+
+    public static MatchString makeG() {
+        return new MatchString("g");
+    }
+
+    public static MatchString makeH() {
+        return new MatchString("h");
+    }
+
+    public static MatchString makeI() {
+        return new MatchString("i");
+    }
+
+    public static MatchString makeJ() {
+        return new MatchString("j");
+    }
+
+    public static MatchString makeK() {
+        return new MatchString("k");
+    }
+
+    public static MatchString makeL() {
+        return new MatchString("l");
+    }
+
+    public static MatchString makeM() {
+        return new MatchString("m");
+    }
+
+    public static MatchString makeN() {
+        return new MatchString("n");
+    }
+
+    public static MatchString makeO() {
+        return new MatchString("o");
+    }
+
+    public static MatchString makeP() {
+        return new MatchString("p");
+    }
+
+    public static MatchString makeQ() {
+        return new MatchString("q");
+    }
+
+    public static MatchString makeR() {
+        return new MatchString("r");
+    }
+
+    public static MatchString makeS() {
+        return new MatchString("s");
+    }
+
+    public static MatchString makeT() {
+        return new MatchString("t");
+    }
+
+    public static MatchString makeU() {
+        return new MatchString("u");
+    }
+
+    public static MatchString makeV() {
+        return new MatchString("v");
+    }
+
+    public static MatchString makeW() {
+        return new MatchString("w");
+    }
+
+    public static MatchString makeX() {
+        return new MatchString("x");
+    }
+
+    public static MatchString makeY() {
+        return new MatchString("y");
+    }
+
+    public static MatchString makeZ() {
+        return new MatchString("z");
+    }
+
+    public static MatchString make1() {
+        return new MatchString("1");
+    }
+
+    public static MatchString make2() {
+        return new MatchString("2");
+    }
+
+    public static MatchString make3() {
+        return new MatchString("3");
+    }
+
+    public static MatchString make4() {
+        return new MatchString("4");
+    }
+
+    public static MatchString make5() {
+        return new MatchString("5");
+    }
+
+    public static MatchString make6() {
+        return new MatchString("6");
+    }
+
+    public static MatchString make7() {
+        return new MatchString("7");
+    }
+
+    public static MatchString make8() {
+        return new MatchString("8");
+    }
+
+    public static MatchString make9() {
+        return new MatchString("9");
+    }
+
+    public static MatchString make0() {
+        return new MatchString("0");
+    }
+
+    public static MatchString makeOrSymbol() {
+        return new MatchString("|");
+    }
+
+    public static MatchString makeAndSymbol() {
+        return new MatchString("&");
+    }
+
+    public static MatchString makeStar() {
+        return new MatchString("*");
+    }
+
+    public static MatchString makePlus() {
+        return new MatchString("+");
+    }
+
+    public static MatchString makeConcatenation(MatchString l, MatchString r) {
+        return new MatchString(l.getString()+r.getString());
+    }
+
+    public static MatchString makeEmpty() {
+        return new MatchString("");
+    }
+
+    public static boolean matches(MatchString m, REstring re) {
+        return matches(m.getString(), re.getString());
     }
 
     /**
